e4e4b45259a06b87a1c36a4ea65adb63e858c3d8
riku <riku.suny@gmail.com>
2021-08-09 14:23:07 +0900

    ログイン後の処理修正

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 import { MainLayout } from "../../components/templates/MainLayout";
+import { useLogin } from "../../hooks/auth";
 
 export const Login = React.memo(() => {
+  const { loginWithGoogle } = useLogin();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const _login = async () => {
+      console.log("login");
+      await loginWithGoogle();
+      history.push("/");
+    };
+    _login();
+  }, [loginWithGoogle, history]);
+
   return (
     <MainLayout>
       <Box>Hello Login</Box>

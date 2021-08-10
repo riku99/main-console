e5f623aa3d177b0a6f552c844613eff86040ca05
riku <riku.suny@gmail.com>
2021-08-10 14:56:01 +0900

    add state

--- src/components/pages/Notifications/Form.tsx
+++ src/components/pages/Notifications/Form.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -14,19 +14,24 @@ import { format } from "date-fns";
 import { Section } from "components/utils/Section";
 
 export const Form = React.memo(() => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
   return (
     <Section w="100%">
       <Flex alignItems="center" justifyContent="space-between">
         <Text fontWeight="bold">お知らせの作成</Text>
-        <Button colorScheme="blue">作成</Button>
+        <Button colorScheme="blue" disabled={!title || !text}>
+          作成
+        </Button>
       </Flex>
       <FormControl mt={3}>
         <FormLabel>タイトル</FormLabel>
-        <Input w="100%" />
+        <Input w="100%" onChange={(e) => setTitle(e.target.value)} />
       </FormControl>
       <FormControl>
         <FormLabel mt={3}>本文</FormLabel>
-        <Textarea h="45vh" multiple />
+        <Textarea h="45vh" multiple onChange={(e) => setText(e.target.value)} />
       </FormControl>
       <Box mt={3}>
         <Text>日付</Text>
